c6b2fae8dcda2260b136f51dbf2bee14e7379c33
qiulin <qlin.dev@outlook.com>
2025-02-10 22:17:56 +0800

    gin server

--- server/gin/handler/wrap.go
+++ server/gin/handler/wrap.go
@@ -1,4 +1,4 @@
-package binding
+package handler
 
 import (
 	"context"
@@ -7,26 +7,40 @@ import (
 	"net/http"
 )
 
-func Handle[IN any, OUT any](handler func(ctx context.Context, in IN) (OUT, error)) gin.HandlerFunc {
+type Option struct {
+	DefaultErrorStatus      int
+	DefaultBadRequestStatus int
+}
+
+var defaultOption = DefaultOption()
+
+func SetOption(o Option) {
+	defaultOption = o
+}
+
+func DefaultOption() Option {
+	return Option{
+		DefaultErrorStatus:      http.StatusOK,
+		DefaultBadRequestStatus: http.StatusOK,
+	}
+}
+
+func Wrap[IN any, OUT any](handler func(ctx context.Context, in IN) (OUT, error)) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var errorRenderFn = DefaultErrorRenderFunc()
 		var in IN
 		if err := c.ShouldBind(in); err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, errorRenderFn(err))
+			c.AbortWithStatusJSON(defaultOption.DefaultBadRequestStatus, errorRenderFn(err))
 			return
 		}
 
 		out, err := handler(c, in)
 		if err != nil {
-			switch e := err.(type) {
-			case *Error:
-				if e.Status != nil {
-					c.AbortWithStatusJSON(*e.Status, errorRenderFn(err))
-				} else {
-					c.AbortWithStatusJSON(http.StatusInternalServerError, errorRenderFn(err))
-				}
-			default:
-				c.AbortWithStatusJSON(http.StatusInternalServerError, errorRenderFn(err))
+			if e, ok := err.(WithStatus); ok {
+				c.JSON(e.Status(), errorRenderFn(err))
+				return
+			} else {
+				c.JSON(defaultOption.DefaultErrorStatus, errorRenderFn(err))
 			}
 		}
 		c.JSON(http.StatusOK, out)
@@ -55,9 +69,13 @@ type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 	Reason  string `json:"reason"`
-	Status  *int   `json:"status"`
+	Status  *int   `json:"-"`
 }
 
 func (e *Error) Error() string {
 	return fmt.Sprintf("[Error %d] %s - %s", e.Code, e.Reason, e.Message)
+}
+
+type WithStatus interface {
+	Status() int
 }
